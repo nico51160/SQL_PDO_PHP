ae1089070b78078fddf7237e771909f15b1858f8
unknown <nicolas.terry@hotmail.fr>
2023-01-09 23:48:57 +0100

    newUpdate

---
+++ captcha/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>mailing</title>
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Recevez nos news</legend>
+               
+
+                <div>
+                    <label for="email">Votre Email</label>
+                    <input type="email" name="email" id="email" placeholder="votre email">
+
+
+
+
+                </div>
+
+                <div>
+                    <label for="captcha">copier le code</label>
+                    <img src="captcha.php" alt="code captcha">
+                    <input type="text" name="captcha" id="captcha" placeholder="copier le code">
+
+
+                </div>
+
+                <input type="submit" value="S'inscrire">
+
+
+
+
+        </fieldset>
+
+    </form>
+</body>
+</html>
