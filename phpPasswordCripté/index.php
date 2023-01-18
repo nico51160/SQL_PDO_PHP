b85bf83399db7131ce9892e1ec6416f94d05340d
unknown <nicolas.terry@hotmail.fr>
2023-01-18 19:29:10 +0100

    newUpdate

---
+++ phpPasswordCripté/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creer votre compte</title>
+</head>
+<body>
+    <section>
+        <form action="" method="post">
+            <h1>Creer votre compte</h1>
+            <input type="text" name="login" placeholder="login">
+            <input type="text" name="pass" placeholder="passeword">
+            <input type="submit" name="create" value="Creer mon compte">
+        </form>
+        <a href="">Acces compte</a>
+    </section>
+</body>
+</html>
